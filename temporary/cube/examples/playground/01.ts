d6c3fe577f3a10aa54e9e5e61de32f7c84a896a6
hudson-oliveira-funttastic <hudson.oliveira@funttastic.com>
2024-03-25 18:31:44 -0300

    turning the methods into functions

--- temporary/cube/examples/playground/01.ts
+++ temporary/cube/examples/playground/01.ts
@@ -1,53 +1,64 @@
 //GET MARKETS
-const responseMarkets = await fetch("https://api.cube.exchange/ir/v0/markets", {
-  method: "GET",
-  headers: {},
-});
-const dataMarkets = await responseMarkets.json();
-console.log(dataMarkets);
+async function responseMarkets() {
+  const response = await fetch("https://api.cube.exchange/ir/v0/markets", {
+    method: "GET",
+    headers: {},
+  });
+  const data = await response.json();
+  console.log(data);
+}
 
 //GET SNAPSHOT
-/* const responseSnapshot = await fetch(
-  "https://api.cube.exchange/md/v0/book/{market_id}/snapshot",
-  {
-    method: "GET",
-    headers: {},
-  }
-);
-const dataSnapshot = await responseSnapshot.json();
-console.log(dataSnapshot);
+async function responseSnapshot() {
+  const response = await fetch(
+    "https://api.cube.exchange/md/v0/book/100036/snapshot",
+    {
+      method: "GET",
+      headers: {},
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+}
 
 // GET RECENT TRADES
-const responseRecentTrades = await fetch(
-  "https://api.cube.exchange/md/v0/book/{market_id}/recent-trades",
-  {
-    method: "GET",
-    headers: {},
-  }
-);
-const datarecentTrades = await responseRecentTrades.json();
-console.log(datarecentTrades);
+async function responseRecentTrades() {
+  const response = await fetch(
+    "https://api.cube.exchange/md/v0/book/{market_id}/recent-trades",
+    {
+      method: "GET",
+      headers: {},
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+}
 
 // GET TICKERS SNAPSHOT
-const responseTickersSnapshot = await fetch(
-  "https://api.cube.exchange/md/v0/tickers/snapshot",
-  {
-    method: "GET",
-    headers: {},
-  }
-);
-const dataTickersSnapshot = await responseTickersSnapshot.json();
-console.log(dataTickersSnapshot);
+async function responseTickersSnapshot() {
+  const response = await fetch(
+    "https://api.cube.exchange/md/v0/tickers/snapshot",
+    {
+      method: "GET",
+      headers: {},
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+}
 
 // GET PARSED TICKERS
-const responseParsedTickers = await fetch(
-  "https://api.cube.exchange/md/v0/parsed/tickers",
-  {
-    method: "GET",
-    headers: {},
-  }
-);
-const dataParsedTickers = await responseParsedTickers.json();
+async function responseParsedTickers() {
+  const response = await fetch(
+    "https://api.cube.exchange/md/v0/parsed/tickers",
+    {
+      method: "GET",
+      headers: {},
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+}
 
 // GET MARKET SYMBOL SNAPSHOT
 const responseMarketSymbolSnapshot = await fetch(
@@ -58,79 +69,73 @@ const responseMarketSymbolSnapshot = await fetch(
   }
 );
 const dataMarketSymbolSnapshot = await responseMarketSymbolSnapshot.json();
+console.log(dataMarketSymbolSnapshot);
 
 // GET MARKET SYMBOL RECENT TRADES
-const responseMarketSymbolRecentTrades = await fetch(
-  "https://api.cube.exchange/md/v0/parsed/book/{market_symbol}/recent-trades",
-  {
-    method: "GET",
-    headers: {},
-  }
-);
-const dataMarketSymbolRecentTrades =
-  await responseMarketSymbolRecentTrades.json();
+async function responseMarketSymbolRecentTrades() {
+  const response = await fetch(
+    "https://api.cube.exchange/md/v0/parsed/book/{market_symbol}/recent-trades",
+    {
+      method: "GET",
+      headers: {},
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+}
 
 // GET ORDERS
-const responseOrders = await fetch("https://api.cube.exchange/os/v0/orders", {
-  method: "GET",
-  headers: {},
-});
-const dataOrders = await responseOrders.json();
-console.log(dataOrders);
+async function responseOrders() {
+  const response = await fetch("https://api.cube.exchange/os/v0/orders", {
+    method: "GET",
+    headers: {},
+  });
+  const data = await response.json();
+  console.log(data);
+}
 
 // DELETE ORDERS
-const responseDeleteOrders = await fetch(
-  "https://api.cube.exchange/os/v0/orders",
-  {
+async function responseDeleteOrders() {
+  const response = await fetch("https://api.cube.exchange/os/v0/orders", {
     method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
-  }
-);
-const dataDeleteOrders = await responseDeleteOrders.json();
-console.log(dataDeleteOrders);
+    headers: {},
+  });
+  const data = await response.json();
+  console.log(data);
+}
 
 // POST ORDERS
-const responseOrdersPost = await fetch(
-  "https://api.cube.exchange/os/v0/orders",
-  {
+async function responseOrdersPost() {
+  const response = await fetch("https://api.cube.exchange/os/v0/orders", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ cancelOnDisconnect: false }),
-  }
-);
-const dataOrdersPost = await responseOrdersPost.json();
-console.log(dataOrdersPost);
+    headers: {},
+  });
+  const data = await response.json();
+  console.log(data);
+}
 
-// DELETE ORDERS
-const responseDeleteOrder = await fetch(
-  "https://api.cube.exchange/os/v0/order",
-  {
+// DELETE ORDER
+async function responseDeleteOrder() {
+  const response = await fetch("https://api.cube.exchange/os/v0/order", {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({}),
-  }
-);
-const dataDeleteOrder = await responseDeleteOrder.json();
-console.log(dataDeleteOrder);
+  });
+  const data = await response.json();
+  console.log(data);
+}
 
 // PATCH ORDER
-const responsePatchOrder = await fetch(
-  "https://api.cube.exchange/os/v0/order",
-  {
+async function responsePatchOrder() {
+  const response = await fetch("https://api.cube.exchange/os/v0/order", {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({}),
-  }
-);
-const dataPatchOrder = await responsePatchOrder.json();
-console.log(dataPatchOrder); */
-export {};
+  });
+  const data = await response.json();
+  console.log(data);
+}
