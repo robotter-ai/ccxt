393a0b38a7f7d3912f2a8dda1323ab86cc45377a
hudson-oliveira-funttastic <hudson.oliveira@funttastic.com>
2024-04-04 19:28:23 -0300

    updating endpoints

--- temporary/cube/examples/playground/01.ts
+++ temporary/cube/examples/playground/01.ts
@@ -1,143 +1,144 @@
 //GET MARKETS
 async function getMarkets() {
-  const response = await fetch("https://api.cube.exchange/ir/v0/markets", {
-    method: "GET",
-    headers: {},
-  });
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch("https://api.cube.exchange/ir/v0/markets", {
+        method: "GET",
+        headers: {},
+    });
+    const data = await response.json();
+    console.log(data);
 }
 
 //GET SNAPSHOT
 async function getSnapshot() {
-  const response = await fetch(
-    "https://api.cube.exchange/md/v0/book/100036/snapshot",
-    {
-      method: "GET",
-      headers: {},
-    }
-  );
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/book/100036/snapshot",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
 }
 
 // GET RECENT TRADES
 async function getRecentTrades() {
-  const response = await fetch(
-    "https://api.cube.exchange/md/v0/book/{market_id}/recent-trades",
-    {
-      method: "GET",
-      headers: {},
-    }
-  );
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/book/100036/recent-trades",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
 }
 
 // GET TICKERS SNAPSHOT
 async function getTickersSnapshot() {
-  const response = await fetch(
-    "https://api.cube.exchange/md/v0/tickers/snapshot",
-    {
-      method: "GET",
-      headers: {},
-    }
-  );
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/tickers/snapshot",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
 }
 
 // GET PARSED TICKERS
 async function getParsedTickers() {
-  const response = await fetch(
-    "https://api.cube.exchange/md/v0/parsed/tickers",
-    {
-      method: "GET",
-      headers: {},
-    }
-  );
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/parsed/tickers",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
 }
 
 // GET MARKET SYMBOL SNAPSHOT
 async function getMarketSymbolSnapshot() {
-  const response = await fetch(
-    "https://api.cube.exchange/md/v0/parsed/book/{market_symbol}/snapshot",
-    {
-      method: "GET",
-      headers: {},
-    }
-  );
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/parsed/book/BTCUSDC/snapshot",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
 }
 
 // GET MARKET SYMBOL RECENT TRADES
 async function getMarketSymbolRecentTrades() {
-  const response = await fetch(
-    "https://api.cube.exchange/md/v0/parsed/book/{market_symbol}/recent-trades",
-    {
-      method: "GET",
-      headers: {},
-    }
-  );
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/parsed/book/BTCUSDC/recent-trades",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
 }
 
 // GET ORDERS
 async function getOrders() {
-  const response = await fetch("https://api.cube.exchange/os/v0/orders", {
-    method: "GET",
-    headers: {},
-  });
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch("https://api.cube.exchange/os/v0/orders", {
+        method: "GET",
+        headers: {},
+    });
+    const data = await response.json();
+    console.log(data);
 }
 
 // DELETE ORDERS
 async function deleteOrders() {
-  const response = await fetch("https://api.cube.exchange/os/v0/orders", {
-    method: "DELETE",
-    headers: {},
-  });
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch("https://api.cube.exchange/os/v0/orders", {
+        method: "DELETE",
+        headers: {},
+    });
+    const data = await response.json();
+    console.log(data);
 }
 
 // POST ORDERS
 async function postOrders() {
-  const response = await fetch("https://api.cube.exchange/os/v0/orders", {
-    method: "POST",
-    headers: {},
-  });
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch("https://api.cube.exchange/os/v0/orders", {
+        method: "POST",
+        headers: {},
+    });
+    const data = await response.json();
+    console.log(data);
 }
 
 // DELETE ORDER
 async function deleteOrder() {
-  const response = await fetch("https://api.cube.exchange/os/v0/order", {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
-  });
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch("https://api.cube.exchange/os/v0/order", {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+    });
+    const data = await response.json();
+    console.log(data);
 }
 
 // PATCH ORDER
 async function patchOrder() {
-  const response = await fetch("https://api.cube.exchange/os/v0/order", {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({}),
-  });
-  const data = await response.json();
-  console.log(data);
+    const response = await fetch("https://api.cube.exchange/os/v0/order", {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+    });
+    const data = await response.json();
+    console.log(data);
 }
+await getMarketSymbolRecentTrades()
--- temporary/cube/examples/playground/02.ts
+++ temporary/cube/examples/playground/02.ts
@@ -1,12 +1,11 @@
-import * as crypto from 'node:crypto';
-import {Buffer} from 'node:buffer';
+import * as crypto from "node:crypto";
+import { Buffer } from "node:buffer";
 
 interface RESTRequest {
   headers?: Record<string, string>;
 }
 
 class Auth {
-
   private apiKey: string;
   private secretKey: string;
 
@@ -29,9 +28,9 @@ class Auth {
     const [signature, timestamp] = this.generateSignature();
 
     return {
-      'x-api-key': this.apiKey,
-      'x-api-signature': signature,
-      'x-api-timestamp': timestamp.toString(),
+      "x-api-key": this.apiKey,
+      "x-api-signature": signature,
+      "x-api-timestamp": timestamp.toString(),
     };
   }
 
@@ -41,41 +40,39 @@ class Auth {
     const timestampBuffer = Buffer.alloc(8);
     timestampBuffer.writeUInt32LE(timestamp, 0);
 
-    const fixedString = 'cube.xyz';
+    const fixedString = "cube.xyz";
 
     const payload = Buffer.concat([
-      Buffer.from(fixedString, 'utf-8'),
+      Buffer.from(fixedString, "utf-8"),
       timestampBuffer,
     ]);
 
-    const secretKeyBytes = Buffer.from(this.secretKey, 'hex');
+    const secretKeyBytes = Buffer.from(this.secretKey, "hex");
     const hmac = crypto
-      .createHmac('sha256', secretKeyBytes)
+      .createHmac("sha256", secretKeyBytes)
       .update(payload)
       .digest();
-    const signatureB64 = hmac.toString('base64');
+    const signatureB64 = hmac.toString("base64");
 
     return [signatureB64, timestamp];
   }
 }
 
-async function usersCheck() {
-  const apiKey = Deno.env.get('CUBE_API_KEY');
-  const apiSecret = Deno.env.get('CUBE_API_SECRET');
+//USERS CHECK
+async function getUsersCheck() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
 
   const auth = new Auth(apiKey, apiSecret);
 
-  const type = 'ir';
-  const baseUrl = `https://api.cube.exchange/${type}/v0`
-  const endpoint = '/users/check';
+  const type = "ir";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/users/check";
   const url = `${baseUrl}${endpoint}`;
-  
+
   let request: any = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    // body: JSON.stringify({}),
+    method: "GET",
+    headers: {},
   };
 
   request = auth.authenticateRequest(request);
@@ -91,4 +88,436 @@ async function usersCheck() {
   }
 }
 
-await usersCheck()
+//USERS INFO
+async function getUsersInfo() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "ir";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/users/info";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "GET",
+    headers: {},
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//USERS POSITIONS
+async function getUsersPositions() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "ir";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/users/positions";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "GET",
+    headers: {},
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+
+//USERS TRANSFERS
+async function getUsersTransfers() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "ir";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/users/transfers";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "GET",
+    headers: {},
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//USERS DEPOSITS
+async function getUsersDeposits() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "ir";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/users/deposits";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "GET",
+    headers: {},
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//USERS WITHDRAWALS
+async function getUsersWithdrawals() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "ir";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/users/withdrawals";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "GET",
+    headers: {},
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//USERS ORDERS
+async function getUsersOrders() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "ir";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/users/orders";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "GET",
+    headers: {},
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//USERS FILLS
+async function getUsersFills() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "ir";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/users/fills";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "GET",
+    headers: {},
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//USERS SUBACCOUNTS
+async function postUsersSubaccounts() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "ir";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/users/subaccounts";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      "accountType": "spot",
+      "name": "text"
+    }),
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//USERS SUBACCOUNTS ID
+async function postUsersSubaccountsId() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "ir";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/users/subaccounts/{subaccount_id}";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      "name": "text"
+    }),
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//GET ORDERS
+async function getOrders() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "os";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/orders";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "GET",
+    headers: {},
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//DELETE ORDERS
+async function deleteOrders() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "os";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/orders";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({}),
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//POST ORDER
+async function postOrder() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "os";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/order";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ "cancelOnDisconnect": false }),
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//DELETE ORDER
+async function deleteOrder() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "os";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/order";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({}),
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+//PATCH ORDER
+async function patchOrder() {
+  const apiKey = Deno.env.get("CUBE_API_KEY");
+  const apiSecret = Deno.env.get("CUBE_API_SECRET");
+
+  const auth = new Auth(apiKey, apiSecret);
+
+  const type = "os";
+  const baseUrl = `https://staging.cube.exchange/${type}/v0`;
+  const endpoint = "/order";
+  const url = `${baseUrl}${endpoint}`;
+
+  let request: any = {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({}),
+  };
+
+  request = auth.authenticateRequest(request);
+
+  try {
+    const response = await fetch(url, request);
+
+    const data = await response.json();
+
+    console.log(data);
+  } catch (exception) {
+    console.error(exception);
+  }
+}
+
+await usersPositions();
