23bd1745e2599631e0175fb6db56a42cd743b89b
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-04-09 20:29:00 -0300

    Improving abstract class and pro and non pro code.

--- ts/src/abstract/cube.ts
+++ ts/src/abstract/cube.ts
@@ -9,28 +9,28 @@ import { implicitReturnType } from '../base/types.js';
 import { Exchange as _Exchange } from '../base/Exchange.js';
 
 interface Exchange {
-    iridiumPublicGetMarkets (params?: {}): Promise<implicitReturnType>;
-    iridiumPrivateGetUsersCheck (params?: {}): Promise<implicitReturnType>;
-    iridiumPrivateGetUsersInfo (params?: {}): Promise<implicitReturnType>;
-    iridiumPrivateGetUsersPositions (params?: {}): Promise<implicitReturnType>;
-    iridiumPrivateGetUsersTransfers (params?: {}): Promise<implicitReturnType>;
-    iridiumPrivateGetUsersDeposits (params?: {}): Promise<implicitReturnType>;
-    iridiumPrivateGetUsersWithdrawals (params?: {}): Promise<implicitReturnType>;
-    iridiumPrivateGetUsersOrders (params?: {}): Promise<implicitReturnType>;
-    iridiumPrivateGetUsersFills (params?: {}): Promise<implicitReturnType>;
-    iridiumPrivatePostUsersSubaccounts (params?: {}): Promise<implicitReturnType>;
-    iridiumPrivatePostUsersSubaccountsSubaccountId (params?: {}): Promise<implicitReturnType>;
-    mendelevPublicGetBookMarketIdSnapshot (params?: {}): Promise<implicitReturnType>;
-    mendelevPublicGetParsedBookMarketSymbolSnapshot (params?: {}): Promise<implicitReturnType>;
-    mendelevPublicGetBookMarketIdRecentTrades (params?: {}): Promise<implicitReturnType>;
-    mendelevPublicGetParsedBookMarketSymbolRecentTrades (params?: {}): Promise<implicitReturnType>;
-    mendelevPublicGetTickersSnapshot (params?: {}): Promise<implicitReturnType>;
-    mendelevPublicGetParsedTickers (params?: {}): Promise<implicitReturnType>;
-    osmiumPrivateGetOrders (params?: {}): Promise<implicitReturnType>;
-    osmiumPrivateDeleteOrders (params?: {}): Promise<implicitReturnType>;
-    osmiumPrivateDeleteOrder (params?: {}): Promise<implicitReturnType>;
-    osmiumPrivatePostOrder (params?: {}): Promise<implicitReturnType>;
-    osmiumPrivatePatchOrder (params?: {}): Promise<implicitReturnType>;
+    restIridiumPublicGetMarkets (params?: {}): Promise<implicitReturnType>;
+    restIridiumPrivateGetUsersCheck (params?: {}): Promise<implicitReturnType>;
+    restIridiumPrivateGetUsersInfo (params?: {}): Promise<implicitReturnType>;
+    restIridiumPrivateGetUsersPositions (params?: {}): Promise<implicitReturnType>;
+    restIridiumPrivateGetUsersTransfers (params?: {}): Promise<implicitReturnType>;
+    restIridiumPrivateGetUsersDeposits (params?: {}): Promise<implicitReturnType>;
+    restIridiumPrivateGetUsersWithdrawals (params?: {}): Promise<implicitReturnType>;
+    restIridiumPrivateGetUsersOrders (params?: {}): Promise<implicitReturnType>;
+    restIridiumPrivateGetUsersFills (params?: {}): Promise<implicitReturnType>;
+    restIridiumPrivatePostUsersSubaccounts (params?: {}): Promise<implicitReturnType>;
+    restIridiumPrivatePostUsersSubaccountsSubaccountId (params?: {}): Promise<implicitReturnType>;
+    restMendelevPublicGetBookMarketIdSnapshot (params?: {}): Promise<implicitReturnType>;
+    restMendelevPublicGetParsedBookMarketSymbolSnapshot (params?: {}): Promise<implicitReturnType>;
+    restMendelevPublicGetBookMarketIdRecentTrades (params?: {}): Promise<implicitReturnType>;
+    restMendelevPublicGetParsedBookMarketSymbolRecentTrades (params?: {}): Promise<implicitReturnType>;
+    restMendelevPublicGetTickersSnapshot (params?: {}): Promise<implicitReturnType>;
+    restMendelevPublicGetParsedTickers (params?: {}): Promise<implicitReturnType>;
+    restOsmiumPrivateGetOrders (params?: {}): Promise<implicitReturnType>;
+    restOsmiumPrivateDeleteOrders (params?: {}): Promise<implicitReturnType>;
+    restOsmiumPrivateDeleteOrder (params?: {}): Promise<implicitReturnType>;
+    restOsmiumPrivatePostOrder (params?: {}): Promise<implicitReturnType>;
+    restOsmiumPrivatePatchOrder (params?: {}): Promise<implicitReturnType>;
 }
 abstract class Exchange extends _Exchange {}
 
