0609e4de2b4fce36bd01918d85e9671d2211ca2a
hudson-oliveira-funttastic <hudson.oliveira@funttastic.com>
2024-04-09 09:35:36 -0300

    Renamed files and endpoints

---
+++ temporary/cube/examples/playground/publicEndpoints.ts
@@ -0,0 +1,89 @@
+//IRIDIUM GET MARKETS
+async function iridiumGetMarkets() {
+    const response = await fetch("https://api.cube.exchange/ir/v0/markets", {
+        method: "GET",
+        headers: {},
+    });
+    const data = await response.json();
+    console.log(data);
+}
+
+// MENDELEV GET SNAPSHOT
+async function mendelevGetSnapshot() {
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/book/100036/snapshot",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
+}
+
+// MENDELEV GET RECENT TRADES
+async function mendelevGetRecentTrades() {
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/book/100036/recent-trades",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
+}
+
+// MENDELEV GET TICKERS SNAPSHOT
+async function mendelevGetTickersSnapshot() {
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/tickers/snapshot",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
+}
+
+// MENDELEV GET PARSED TICKERS
+async function mendelevGetParsedTickers() {
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/parsed/tickers",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
+}
+
+// MENDELEV GET MARKET SYMBOL SNAPSHOT
+async function mendelevGetMarketSymbolSnapshot() {
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/parsed/book/BTCUSDC/snapshot",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
+}
+
+// MENDELEV GET MARKET SYMBOL RECENT TRADES
+async function mendelevGetMarketSymbolRecentTrades() {
+    const response = await fetch(
+        "https://api.cube.exchange/md/v0/parsed/book/BTCUSDC/recent-trades",
+        {
+            method: "GET",
+            headers: {},
+        }
+    );
+    const data = await response.json();
+    console.log(data);
+}
+
+await iridiumGetMarkets()
