4dd54b265728fb54c0d80f0bd8028136a53cbed2
Darley Araújo Silva <darley.silva@funttastic.com>
2024-04-10 22:43:18 -0300

    Fixing 'fetchOrderBook' method.

--- ts/src/cube.ts
+++ ts/src/cube.ts
@@ -619,28 +619,11 @@ export default class cube extends Exchange {
          * @returns {object} A dictionary of [order book structures]{@link https://docs.ccxt.com/#/?id=order-book-structure} indexed by market symbols
          */
         await this.loadMarkets ();
-        const request = {};
-        // const response = await this.restMendelevPublicGetBookMarketIdSnapshot(this.extend(request, params));
-        //
-        // {
-        //   result: {
-        //       levels: [
-        //           {
-        //               price: 49301,
-        //               quantity: 33350,
-        //               side: 0,
-        //           },
-        //           {
-        //               price: 50535,
-        //               quantity: 63259,
-        //               side: 1,
-        //           }
-        //       ],
-        //       lastTransactTime: 1711543858131858200,
-        //       lastTradePrice: 48676,
-        //   },
-        // }
-        //
+        const marketId = symbol.toLowerCase ();
+        const market = this.market (marketId);
+        const marketInfo = this.safeDict (market, 'info');
+        const symbolFromInfo = this.safeString (marketInfo, 'symbol');
+        const request = { 'market_symbol': symbolFromInfo };
         const response = await this.restMendelevPublicGetParsedBookMarketSymbolSnapshot (this.extend (request, params));
         //
         // {
@@ -668,12 +651,12 @@ export default class cube extends Exchange {
             'bids': rawBids,
             'asks': rawAsks,
         };
-        const orderbook = this.parseOrderBook (rawOrderbook, symbol);
-        return orderbook;
+        const timestamp = this.safeTimestamp (this.safeDict (response, 'result'), 'timestamp');
+        return this.parseOrderBook (rawOrderbook, symbol, timestamp, 'bids', 'asks');
     }
 
     parseBidsAsks (bidasks, priceKey: IndexType = 0, amountKey: IndexType = 1, countOrIdKey: IndexType = 2): any[] {
-        throw Error ('Not implemented!'); // TODO fix!!!
+        return bidasks;
     }
 
     async fetchTrades (symbol: string, since: Int = undefined, limit: Int = undefined, params = {}) {
