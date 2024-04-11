ab0018db9d50d2d8ea860b65a9b69aad848e082e
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-04-10 21:22:09 -0300

    Improving cube.js

--- temporary/cube/examples/playground/javascript/ccxt/cube.js
+++ temporary/cube/examples/playground/javascript/ccxt/cube.js
@@ -5,11 +5,19 @@ function log(target) {
 }
 
 async function test() {
-    log(ccxt.exchanges)
-    const cube = new ccxt.cube();
-    log(cube)
-    const markets = await cube.fetchMarkets()
-    log(markets)
+    // log(ccxt.exchanges)
+    const cube = new ccxt.cube()
+    cube.apiKey = process.env['CUBE_API_KEY']
+    cube.secret = process.env['CUBE_API_SECRET']
+    const cubePro = new ccxt.pro.cube()
+    cubePro.apiKey = process.env['CUBE_API_KEY']
+    cubePro.secret = process.env['CUBE_API_SECRET']
+    // log(cube)
+    // log(cubePro)
+    const response = await cube.fetchBalance()
+    log(response)
+    // const response = await cubePro.watchOrderBook('tbtctusdc')
+    // log(response)
 }
 
 test()
