a09a40e404ff45fd07ddc27c6be25803845ad3ac
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-04-19 14:44:42 -0300

    Reverting changes or removing files not acceptable for a PR.

--- js/src/pro/cube.d.ts
+++
@@ -1,6 +0,0 @@
-import cubeRest from '../cube.js';
-import { Int, OrderBook } from '../base/types.js';
-export default class cube extends cubeRest {
-    describe(): any;
-    watchOrderBook(symbol: string, limit?: Int, params?: {}): Promise<OrderBook>;
-}
