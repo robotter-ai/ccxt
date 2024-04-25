a571ba371e6036c736499edb9491f0291f07e840
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-04-25 18:25:41 -0300

    Updating cube pro.

--- ts/src/pro/cube.ts
+++ ts/src/pro/cube.ts
@@ -1,17 +1,32 @@
 
 // ----------------------------------------------------------------------------
-
-import cubeRest from '../cube.js';
-import { Int, OrderBook } from '../base/types.js';
+// TODO revert imports to js!!!
+import cubeRest from '../cube.ts';
+import { Int, OrderBook } from '../base/types.ts';
 
 // -----------------------------------------------------------------------------
 
 export default class cube extends cubeRest {
     describe () {
-        // TODO check all info!!!
         return this.deepExtend (super.describe (), {
             'has': {
                 'ws': true,
+                'cancelOrderWs': false,
+                'cancelOrdersWs': false,
+                'cancelAllOrdersWs': false,
+                'createOrderWs': false,
+                'editOrderWs': false,
+                'fetchBalanceWs': false,
+                'fetchDepositsWs': false,
+                'fetchMarketsWs': false,
+                'fetchMyTradesWs': false,
+                'fetchOHLCVWs': false,
+                'fetchOpenOrdersWs': false,
+                'fetchOrderWs': false,
+                'fetchOrdersWs': false,
+                'fetchTradesWs': false,
+                'fetchTradingFeesWs': false,
+                'fetchWithdrawalsWs': false,
                 'watchBalance': false,
                 'watchMyTrades': false,
                 'watchOHLCV': false,
@@ -25,53 +40,31 @@ export default class cube extends cubeRest {
                 'watchTickers': false,
                 'watchTrades': false,
                 'watchTradesForSymbols': false,
-                'createOrderWs': false,
-                'editOrderWs': false,
-                'cancelOrderWs': false,
-                'cancelOrdersWs': false,
-                'cancelAllOrdersWs': false,
-                'fetchBalanceWs': false,
-                'fetchDepositsWs': false,
-                'fetchMarketsWs': false,
-                'fetchMyTradesWs': false,
-                'fetchOHLCVWs': false,
-                'fetchOpenOrdersWs': false,
-                'fetchOrderWs': false,
-                'fetchOrdersWs': false,
-                'fetchTradesWs': false,
-                'fetchTradingFeesWs': false,
-                'fetchWithdrawalsWs': false,
-            },
-            'urls': {
-                'api': {
-                    'ws': {
-                        'production': {
-                            'iridium': 'wss://api.cube.exchange/ir',
-                            'mendelev': 'wss://api.cube.exchange/md',
-                            'osmium': 'wss://api.cube.exchange/os',
-                        },
-                        'staging': {
-                            'iridium': 'wss://staging.cube.exchange/ir',
-                            'mendelev': 'wss://staging.cube.exchange/md',
-                            'osmium': 'wss://staging.cube.exchange/os',
-                        },
-                    },
-                },
             },
             'options': {
-                'environment': 'production',
                 'api': {
                     'ws': {
                         'mendelev': {
                             'public': {
-                                'orderbook': '/book/:market_id',
+                                'orderbook': '/book/{marketId}',
                                 'orderbookTops': '/tops',
+                            },
+                        },
+                        'os': {
+                            'private': {
+                                'root': '/',
                             },
                         },
                     },
                 },
             },
         });
+    }
+
+    getWebsocketUrl (system: string, privacy: string, path: string, params = {}) {
+        const environment = this.options['environment'];
+        path = this.implodeParams (path, params);
+        return this.urls['api']['ws'][environment][system] + this.options['api']['ws'][environment][privacy][path];
     }
 
     async watchOrderBook (symbol: string, limit: Int = undefined, params = {}): Promise<OrderBook> {
@@ -85,18 +78,19 @@ export default class cube extends cubeRest {
          * @param {object} [params] extra parameters specific to the exchange API endpoint
          * @returns {object} A dictionary of [order book structures]{@link https://docs.ccxt.com/#/?id=order-book-structure} indexed by market symbols
          */
-        await this.loadMarkets ();
-        const environment = this.options['environment'];
-        const marketId = symbol.toLowerCase ();
-        const market = this.market (marketId);
-        symbol = this.safeSymbol (marketId, market);
-        const url = this.urls['api']['ws'][environment]['mendelev'] + this.options['api']['ws']['mendelev']['public']['orderbook'];
-        const requestId = '';
-        const subParams = [];
+        const meta = await this.fetchMarketMeta (symbol);
+        symbol = this.safeString (meta, 'symbol');
+        const marketId = this.safeString (meta, 'marketId');
+        // const market = this.safeString (meta, 'market');
+        const url = this.getWebsocketUrl ('mendelev', 'public', 'orderbook', { 'marketId': marketId });
+        const klines = this.safeValue (params, 'klines', 1);
         const request = {
-            'method': 'SUBSCRIBE',
-            'params': subParams,
-            'id': requestId,
+            'mbp': true,
+            'mbo': false,
+            'trades': true,
+            'summary': true,
+            'klines': 1,
+            'marketsIds': 200047,
         };
         const messageHash = '';
         return await this.watch (url, messageHash, request, messageHash);
