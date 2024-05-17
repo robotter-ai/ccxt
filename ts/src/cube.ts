27204a6b0ee549fe7303db987fda7c9962df76b3
Danilo Araújo Silva <danilo.silva@funttastic.com>
2024-05-17 14:07:29 -0300

    Updating cube.ts

--- ts/src/cube.ts
+++ ts/src/cube.ts
@@ -1963,7 +1963,7 @@ export default class cube extends Exchange {
         [ tag, params ] = this.handleWithdrawTagAndParams (tag, params);
         await this.fetchMarketMeta ();
         const request = {
-            'amount': amount,
+            'amount': this.numberToString (amount),
             'destination': address,
             'assetId': code,
         };
